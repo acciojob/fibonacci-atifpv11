2bcc9a6ec14fad44e2f2ac8fad725494fcbeb490
atifpv11 <115897151+atifpv11@users.noreply.github.com>
2024-09-13 20:47:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+	if (num === 0) {
+        return 0; // Fibonacci(0) is 0
+    } 
+	else if (num === 1) {
+        return 1; // Fibonacci(1) is 1
+    }
+
+    // Recursive case: Fibonacci(n) = Fibonacci(n-1) + Fibonacci(n-2)
+    return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 module.exports = fibonacci;
